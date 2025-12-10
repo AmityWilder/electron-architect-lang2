12b05a8fdd494775d061cc5c0822c0f19ecc3599
Amy Wilder <74995093+HenryWilder@users.noreply.github.com>
2025-12-09 20:44:51 -0500

    Enable providing source code from file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //! current version is basically just https://en.wikipedia.org/wiki/Brainfuck
 
-use std::collections::BTreeMap;
+use clap::Parser;
+use std::{collections::BTreeMap, path::PathBuf};
 
 macro_rules! dprint {
     ($($args:tt)*) => {
@@ -427,89 +428,44 @@ impl<'a, 'b, 'c, I: std::io::Read, O: std::io::Write> Environment<'a, 'b, 'c, I,
     }
 }
 
+#[derive(Debug, Parser)]
+pub struct Cli {
+    /// How much memory to give the application
+    #[arg(short, long, default_value = "2048")]
+    mem: usize,
+
+    /// Path to the code file
+    path: PathBuf,
+}
+
 fn main() {
+    let Cli { mem, path } = Cli::parse();
     let code = Instruction::vec_from_str(
-        r#"
-[ This program prints "Hello World!" and a newline to the screen; its
-  length is 106 active command characters. [It is not the shortest.]
-
-  This loop is an "initial comment loop", a simple way of adding a comment
-  to a BF program such that you don't have to worry about any command
-  characters. Any ".", ",", "+", "-", "<" and ">" characters are simply
-  ignored, the "[" and "]" characters just have to be balanced. This
-  loop and the commands it contains are ignored because the current cell
-  defaults to a value of 0; the 0 value causes this loop to be skipped.
-]
-++++++++                Set Cell #0 to 8
-[
-    >++++               Add 4 to Cell #1; this will always set Cell #1 to 4
-    [                   as the cell will be cleared by the loop
-        >++             Add 2 to Cell #2
-        >+++            Add 3 to Cell #3
-        >+++            Add 3 to Cell #4
-        >+              Add 1 to Cell #5
-        <<<<-           Decrement the loop counter in Cell #1
-    ]                   Loop until Cell #1 is zero; number of iterations is 4
-    >+                  Add 1 to Cell #2
-    >+                  Add 1 to Cell #3
-    >-                  Subtract 1 from Cell #4
-    >>+                 Add 1 to Cell #6
-    [<]                 Move back to the first zero cell you find; this will
-                        be Cell #1 which was cleared by the previous loop
-    <-                  Decrement the loop Counter in Cell #0
-]                       Loop until Cell #0 is zero; number of iterations is 8
-
-The result of this is:
-Cell no :   0   1   2   3   4   5   6
-Contents:   0   0  72 104  88  32   8
-Pointer :   ^
-
->>.                     Cell #2 has value 72 which is 'H'
->---.                   Subtract 3 from Cell #3 to get 101 which is 'e'
-+++++++..+++.           Likewise for 'llo' from Cell #3
->>.                     Cell #5 is 32 for the space
-<-.                     Subtract 1 from Cell #4 for 87 to give a 'W'
-<.                      Cell #3 was set to 'o' from the end of 'Hello'
-+++.------.--------.    Cell #3 for 'rl' and 'd'
->>+.                    Add 1 to Cell #5 gives us an exclamation point
->++.                    And finally a newline from Cell #6
-"#,
+        &std::fs::read_to_string(path).expect("failed to read code from file"),
     );
 
-    for item in &code {
-        dprint!("{item}");
+    #[cfg(debug_assertions)]
+    {
+        for item in &code {
+            dprint!("{item}");
+        }
+        dprintln!();
+        dprintln!("len: {}", code.len());
     }
-    dprintln!();
-    dprintln!("len: {}", code.len());
 
     match Program::compile(&code) {
         Err(e) => eprintln!("compile error: {e}"),
         Ok(prgm) => {
-            let input = b"apple";
-            let mut output = [0u8; 32];
-            let mut memory = [0u8; 128];
+            let mut memory = vec![0u8; mem];
 
-            let mut env = Environment::new(&mut memory, &prgm, &input[..], &mut output[..]);
+            let mut env = Environment::new(&mut memory, &prgm, std::io::stdin(), std::io::stdout());
             let err = env.run().err();
             if err.is_some() {
                 dprintln!(" ERR");
             }
-            let (o, e) = match str::from_utf8(&output) {
-                Ok(o) => (o, None),
-                Err(e) => (
-                    // SAFETY: `valid_up_to` is by definition the length of the string that is valid UTF-8.
-                    unsafe { str::from_utf8_unchecked(&output[..e.valid_up_to()]) },
-                    Some(e),
-                ),
-            };
 
-            dprintln!("\noutput:");
-            println!("{o}");
             if let Some(err) = err {
                 eprintln!("runtime error: {err}");
-            }
-            if let Some(e) = e {
-                eprintln!("utf-8 error: {e}");
             }
         }
     }
